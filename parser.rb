852799d9fef2a34bd0dc7f6d6bdda28bd1a58060
Lyn Levenick <lyn.levenick@gmail.com>
2018-09-20 01:48:53 -0700

    Add yardoc to parser

--- parser.rb
+++ parser.rb
@@ -3,15 +3,20 @@
 require "json"
 require "strscan"
 
-# Transforms a string into a form
+# Namespace for functions parsing strings into lisp-style forms
 module Parser
+  # Error raised when parsing expects content but finds end of file
   class UnexpectedEofError < StandardError
   end
+  # Error raised when parsing receives unexpected or malformed content
   class BadParseError < StandardError
   end
 
-  IGNORED_REGEXP = /\A(?:\s+|;.*$)*/ # comments, whitespace
+  # Matches comments and whitespace
+  IGNORED_REGEXP = /\A(?:\s+|;.*$)*/
+  # Matches numbers, no distinction between integers and floats
   NUMBER_REGEXP = /\A(?:-)?[0-9]*(?:.[0-9]+)?(?:e[0-9]+(?:.[0-9]+)?)?\z/
+  # Matches ruby-style-delimited regular expression syntax
   REGEXP_REGEXP = /
     \A
     (?:
@@ -19,8 +24,10 @@ module Parser
     | %r\{(?:\\.|[^\\}])*}
     )[imxo]*
   /mx
+  # Matches strings, including escaping
   STRING_REGEXP = /\A"(?:\\.|[^\\"])*"\z/
 
+  # Matches any Sapphire token, skipping whitespace and comments
   TOKEN_REGEXP = /
     #{IGNORED_REGEXP}
     (
@@ -32,6 +39,11 @@ module Parser
     )
   /x
 
+  # Parses a top-level lisp form into ruby objects
+  # @param [String] str string to parse forms from
+  # @return [Symbol, Array<Object>]
+  # @raise [UnexpectedEofError]
+  # @raise [BadParseError]
   def self.parse(str)
     tokens = lex(str)
     forms = [:begin]
@@ -39,12 +51,20 @@ module Parser
     forms.freeze
   end
 
+  # Parses a lisp form into a ruby object
+  # @todo Don't lex the entire string just to read a form off the front
+  # @param [String] str string to parse form from
+  # @return [Array<Object>]
+  # @raise [UnexpectedEofError]
+  # @raise [BadParseError]
   def self.parse_single_form(str)
     tokens = lex(str)
     read_form!(tokens).freeze
   end
 
-  private_class_method def self.lex(str)
+  # @param [String] str
+  # @return [Array<String>] tokens
+  def self.lex(str)
     scanner = StringScanner.new(str)
     tokens = []
 
@@ -60,8 +80,14 @@ module Parser
 
     tokens
   end
+  private_class_method :lex
 
-  private_class_method def self.parse_regexp_options(options)
+  # @param [String] options
+  # @return [Numeric] regexp options suitable to pass to {Regexp.new}
+  # @example
+  #   nil => 0
+  #   "ix" => {Regexp::IGNORECASE} | {Regexp::EXTENDED}
+  def self.parse_regexp_options(options)
     options&.chars&.reduce(0) { |acc, c|
       case c
       when "i" then acc | Regexp::IGNORECASE
@@ -71,8 +97,13 @@ module Parser
       end
     }
   end
+  private_class_method :parse_regexp_options
 
-  private_class_method def self.parse_regexp_source(source)
+  # @param [String] source full regexp token
+  # @return [Regexp] parsed regexp
+  # @example
+  #   "/abc/i" => /abc/i
+  def self.parse_regexp_source(source)
     case source[0]
     when "/"
       split_idx = source.rindex("/")
@@ -85,14 +116,22 @@ module Parser
 
     Regexp.new(re, parse_regexp_options(source[split_idx + 1..-1]))
   end
+  private_class_method :parse_regexp_source
 
-  private_class_method def self.parse_string_source(source)
+  # @param [String] source
+  # @return [String] parsed string
+  # @example
+  #   '"a\\"b"' #=> 'a"b'
+  def self.parse_string_source(source)
     JSON.parse(source)
   end
+  private_class_method :parse_string_source
 
   # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/MethodLength
-  private_class_method def self.read_form!(tokens)
+  # @param [Array<String>] tokens
+  # @return [Object] tokens parsed into ruby objects
+  def self.read_form!(tokens)
     raise UnexpectedEofError if tokens.empty?
 
     token = tokens.shift
@@ -132,8 +171,11 @@ module Parser
   end
   # rubocop:enable Metrics/MethodLength
   # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity
+  private_class_method :read_form!
 
-  private_class_method def self.read_list!(tokens)
+  # @param [Array<String>] tokens
+  # @return [Array<Object>] tokens read into a list as forms
+  def self.read_list!(tokens)
     res = []
     until tokens.first == ")"
       raise UnexpectedEofError if tokens.first.nil?
@@ -143,4 +185,5 @@ module Parser
     tokens.shift
     res
   end
+  private_class_method :read_list!
 end
