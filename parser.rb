d2b68680937f1c188eb44883107877eaf5edc8d0
Lyn Levenick <lyn.levenick@gmail.com>
2018-09-19 23:43:49 -0700

    Add regular expression parsing, small refactors

--- parser.rb
+++ parser.rb
@@ -10,9 +10,26 @@ module Parser
   class BadParseError < StandardError
   end
 
-  @token_regexp = /^(?:\s+|;.*$)*(,@|[()'`,]|"(?:\\.|[^\\"])*"|[^\s;()'`,]+)/
-  @number_regexp = /^(?:-)?[0-9]*(?:.[0-9]+)?(?:e[0-9]+(?:.[0-9]+)?)?$/
-  @string_regexp = /^"(?:\\.|[^\\"])*"$/
+  NUMBER_REGEXP = /\A(?:-)?[0-9]*(?:.[0-9]+)?(?:e[0-9]+(?:.[0-9]+)?)?\z/
+  REGEXP_REGEXP = /
+    \A
+    (?:
+      \/(?:\\.|[^\\\/])*\/
+    | %r\{(?:\\.|[^\\}])*}
+    )[imxo]*
+  /mx
+  STRING_REGEXP = /\A"(?:\\.|[^\\"])*"\z/
+
+  TOKEN_REGEXP = /
+    (?:\s+|;.*$)*       # ignore comments, whitespace
+    (
+      ,@ | [()'`,]      # unquote-splicing, parens, quote, quasiquote, unquote
+    | #{NUMBER_REGEXP}
+    | #{REGEXP_REGEXP}
+    | #{STRING_REGEXP}
+    | [^\s;()'`,]+      # identifiers
+    )
+  /x
 
   def self.parse(str)
     tokens = lex(str)
@@ -21,24 +38,63 @@ module Parser
     forms.freeze
   end
 
+  def self.parse_single_form(str)
+    tokens = lex(str)
+    read_form!(tokens).freeze
+  end
+
   private_class_method def self.lex(str)
     scanner = StringScanner.new(str)
     tokens = []
 
     until scanner.eos?
-      break unless scanner.scan(@token_regexp)
+      break unless scanner.scan(TOKEN_REGEXP)
+
       tokens << scanner[1]
     end
 
     tokens
   end
 
+  private_class_method def self.parse_regexp_source(source)
+    case source[0]
+    when "/"
+      split_idx = source.rindex("/")
+      re = source[1..split_idx - 1]
+    when "%"
+      split_idx = source.rindex("}")
+      re = source[3..split_idx - 1]
+    else
+      raise BadParseError
+    end
+
+    options = source[split_idx + 1..-1]&.chars&.reduce(0) { |acc, c|
+      case c
+      when "i"
+        acc | Regexp::IGNORECASE
+      when "m"
+        acc | Regexp::MULTILINE
+      when "x"
+        acc | Regexp::EXTENDED
+      else
+        acc
+      end
+    }
+
+    Regexp.new(re, options)
+  end
+
+  private_class_method def self.parse_string_source(source)
+    JSON.parse(source)
+  end
+
   private_class_method def self.read_form!(tokens)
     raise UnexpectedEofError if tokens.empty?
+
     token = tokens.shift
 
     case token
-    when ""
+    when "", ")"
       raise BadParseError
     when "("
       read_list!(tokens)
@@ -56,10 +112,12 @@ module Parser
       [:unquote, read_form!(tokens)]
     when ",@"
       [:'unquote-splicing', read_form!(tokens)]
-    when @number_regexp
+    when NUMBER_REGEXP
       token.to_f
-    when @string_regexp
-      JSON.parse(token)
+    when REGEXP_REGEXP
+      parse_regexp_source(token)
+    when STRING_REGEXP
+      parse_string_source(token)
     else
       token.to_sym
     end.freeze
@@ -69,6 +127,7 @@ module Parser
     res = []
     until tokens.first == ")"
       raise UnexpectedEofError if tokens.first.nil?
+
       res << read_form!(tokens)
     end
     tokens.shift
